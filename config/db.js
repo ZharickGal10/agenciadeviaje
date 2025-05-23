934eceb1e655cc957399bacd5aa7fa0ef2d3cf71
Yeison Duarte <111292822+Yeison0134@users.noreply.github.com>
2025-05-22 20:14:09 -0500

    agenciaarreglo

--- config/db.js
+++ config/db.js
@@ -1,19 +1,24 @@
-import Sequelize  from "sequelize";
+import Sequelize from 'sequelize';
 
-const db = new Sequelize('pro', 'root', '', {
-    host: 'localhost',
-    dialect: '3306',
+const db = new Sequelize(
+  'bmcgx1ysujeuvkkfzssp', // base de datos
+  'ud1mnm3ww0pqslo8',     // usuario
+  'ud1mnm3ww0pqslo8', // contraseña
+  {
+    host: 'bmcgx1ysujeuvkkfzssp-mysql.services.clever-cloud.com', // host
     dialect: 'mysql',
+    port: process.env.DB_PORT || 3306, // agregar explícitamente el puerto si es necesario
     define: {
-        timestamps: false
+      timestamps: false
     },
     pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
     },
-    operatorAliases: false
-});
+    operatorsAliases: false
+  }
+);
 
 export default db;
